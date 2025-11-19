f2d29d809e62cb90d3f936f33b30c0f3280ae89a
exitus1 <46197055+exitus1@users.noreply.github.com>
2025-11-19 12:17:23 -0500

    Delete decred-dcrd directory

--- decred-dcrd/exports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export APP_DECRED_NODE_RPC_PORT="9109"
-export APP_DECRED_NODE_P2P_PORT="9108"
-export APP_DECRED_NODE_RPC_USER="umbrel"
-export APP_DECRED_NODE_RPC_PASS=$(head -c 32 /dev/urandom | base64 | tr -dc 'a-zA-Z0-9' | head -c 32)
-
-
